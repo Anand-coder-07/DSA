9ad8e6b70c10fa1157d7aacc240e0d8d24761582
Anand Singh <anandsingh9107018@gmail.com>
2026-07-16 22:55:36 +0530

    Time: 48 ms (26.35%), Space: 53.2 MB (78.93%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    
+
+    let first = 0;
+    let second = 1;
+
+    while(true) {
+        yield first;
+        [first,second] = [second,first+second];
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
