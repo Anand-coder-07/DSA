7fcf292b54b766521c9690bcc32256abf245eaae
Anand Singh <anandsingh9107018@gmail.com>
2026-07-16 15:47:23 +0530

    Time: 2 ms (78.42%), Space: 7.7 MB (65.19%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fib(int n) {
+
+       if(n==0) return 0;
+       if(n==1) return 1;
+
+       int first = 0;
+       int second = 1;
+
+       int next;
+
+       int i = 2;
+        while (i <= n) {
+            next = first +second;
+            first = second;
+            second = next;
+            i++;
+        }
+        return next; 
+        
+    }
+};
