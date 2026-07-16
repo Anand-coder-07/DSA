b753a799eeebd5fffa913f8bd13b31c38a458f3f
Anand Singh <anandsingh9107018@gmail.com>
2026-07-16 17:06:58 +0530

    Time: 0 ms (100%), Space: 8 MB (1.62%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string winningPlayer(int x, int y) {
+
+        int count = 0;
+
+        while(x >=1 && y >= 4) {
+
+            x = x-1;
+            y = y-4;
+            count++;
+        }
+        if(count%2!=0) {
+            return "Alice";
+        }
+        return "Bob";
+    }
+};
