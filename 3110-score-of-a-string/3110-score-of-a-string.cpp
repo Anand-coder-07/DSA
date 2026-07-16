4160303ab2c4d912b0ebac6a3d542368ee35866f
Anand Singh <anandsingh9107018@gmail.com>
2026-07-17 00:50:11 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.48%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.size();
+
+        int sum = 0;
+
+        for(int i = 1; i < n; i++) {
+
+            sum = sum + abs(s[i-1]-s[i]);
+        }
+        return sum;
+        
+    }
+};
